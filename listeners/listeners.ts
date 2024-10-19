a24796b9f38aa6f0cece4694d8d25e8b4e169434
Emel <ebu.pro@gmail.com>
2024-10-19 02:33:17 +0200

    improve

--- listeners/listeners.ts
+++ listeners/listeners.ts
@@ -1,5 +1,5 @@
 import { Token, TOKEN_PROGRAM_ID } from '@raydium-io/raydium-sdk';
-import { Connection, PublicKey } from '@solana/web3.js'; 
+import { Connection, PublicKey, VersionedTransactionResponse } from '@solana/web3.js'; 
 import { EventEmitter } from 'events';
 import Client, {
   CommitmentLevel,
@@ -16,6 +16,7 @@ import { logger } from '../helpers';
 import { SubscribeRequestPing } from '@triton-one/yellowstone-grpc/dist/grpc/geyser'; 
 
 import { TransactionFormatter } from '../utils/transaction-formater';  
+import { RaydiumAmmParser } from '../utils/raydium-amm-parser';
 
 interface SubscribeRequest {
   accounts: { [key: string]: SubscribeRequestFilterAccounts };
@@ -30,7 +31,9 @@ interface SubscribeRequest {
   ping?: SubscribeRequestPing | undefined;
 }
  
-const TXN_FORMATTER = new TransactionFormatter();  
+const TXN_FORMATTER = new TransactionFormatter();
+const RAYDIUM_PARSER = new RaydiumAmmParser();
+
 export class GrpcListeners extends EventEmitter {
   private accountStream: any;  
 
@@ -56,30 +59,24 @@ export class GrpcListeners extends EventEmitter {
     }); 
 
     const req: SubscribeRequest = {
+      accounts: {},
       slots: {},
-      accounts: {
+      transactions: {
         mpox: {
-          account: [],
-          owner: [TOKEN_PROGRAM_ID.toBase58()],
-          filters: [
-            {
-              tokenAccountState: true,
-            },
-            {
-              memcmp: {
-                offset: String(0),
-                base58: config.walletPublicKey.toBase58(),
-              },
-            },
-          ],
+          vote: false,
+          failed: false,
+          signature: undefined,
+          accountInclude: [], //input wallet
+          accountExclude: [],
+          accountRequired: ['675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8', config.walletPublicKey.toString()],
         },
       },
-      transactions: {},
       transactionsStatus: {},
+      entry: {},
       blocks: {},
       blocksMeta: {},
-      entry: {},
       accountsDataSlice: [],
+      ping: undefined,
       commitment: CommitmentLevel.CONFIRMED,
     };
 
@@ -91,13 +88,66 @@ export class GrpcListeners extends EventEmitter {
   }
 
   private async handlePoolStreamData(chunk: any) { 
-    if(chunk?.account?.account){
-      const txn = await TXN_FORMATTER.formTransactionFromJson(
-       chunk?.account?.account,
-      ) 
+    if (chunk?.transaction) {
+      const txn = TXN_FORMATTER.formTransactionFromJson(
+        chunk.transaction,
+        Date.now(),
+      );
+      const decodedRaydiumIxs = this.decodeRaydiumTxn(txn);
 
-      this.emit('new_buy', txn);
-    } 
+      if (!decodedRaydiumIxs?.length) return;
+      const createPoolIx = decodedRaydiumIxs.find((decodedRaydiumIx) => {
+        if (
+          decodedRaydiumIx.name === "swapIn" ||
+          decodedRaydiumIx.name === "swapOut"
+        ) {
+          return decodedRaydiumIx;
+        }
+      });
+       if (createPoolIx) {
+        const info  = this.getMintToken(chunk);
+        const stringify : any = this.stringifyWithBigInt(createPoolIx.args);
+        console.log(
+          `Signature: ${txn.transaction.signatures[0]}
+           CA : ${info.ca}
+           Pool Info : ${stringify}
+           Owner : ${info.signer}
+          `
+        );
+      }
+    }
+  }
+
+  private stringifyWithBigInt(obj: any): string {
+    return JSON.stringify(obj, (key, value) => 
+      typeof value === 'bigint' ? value.toString() : value);
+  }
+
+  private decodeRaydiumTxn(tx: VersionedTransactionResponse) {
+    if (tx.meta?.err) return;
+  
+    const allIxs = TXN_FORMATTER.flattenTransactionResponse(tx);
+  
+    const raydiumIxs = allIxs.filter((ix) =>
+      ix.programId.equals(new PublicKey('675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8')),
+    );
+  
+    const decodedIxs = raydiumIxs.map((ix) =>
+      RAYDIUM_PARSER.parseInstruction(ix),
+    );
+  
+    return decodedIxs;
+  }
+  
+  private getMintToken(tx: any){
+    const data : any[] = tx.transaction.transaction.meta.preTokenBalances;
+    const filter = data.filter((t)=> t.mint !== "So11111111111111111111111111111111111111112")
+    const ca = filter[0].mint;
+    const signer = filter[0].owner;
+     return {
+      ca,
+      signer
+    };
   }
   
   // private async decodeRaydiumTxn(tx: VersionedTransactionResponse) {
--- utils/transaction-formater.ts
+++ utils/transaction-formater.ts
@@ -12,151 +12,155 @@ import {
   VersionedMessage,
   VersionedTransactionResponse,
 } from "@solana/web3.js";
-import { accountDeserialize } from "./deserializingAccount";
 
 export class TransactionFormatter {
-  public async formTransactionFromJson(
+  public formTransactionFromJson(
     data: any,
-  ) {
-    const signatures = base58.encode(
-      Buffer.from(data.txnSignature,"base64")
-    )
-   const publicKey = base58.encode(
-    Buffer.from(data.pubkey,'base64')
-     )
-   const owner = base58.encode(
-    Buffer.from(data.owner,'base64')
-   )
-  const info = await accountDeserialize(data.data)
-   
+    time: number,
+  ): VersionedTransactionResponse {
+    const rawTx = data["transaction"];
+
+    const slot = data.slot;
+    const version = rawTx.transaction.message.versioned ? 0 : "legacy";
+
+    const meta = this.formMeta(rawTx.meta);
+    const signatures = rawTx.transaction.signatures.map((s: Buffer) =>
+      base58.encode(s),
+    );
+
+    const message = this.formTxnMessage(rawTx.transaction.message);
 
     return {
-      publicKey,
-      signatures,
-      owner,
-      info
+      slot,
+      version,
+      blockTime: time,
+      meta,
+      transaction: {
+        signatures,
+        message,
+      },
     };
   }
 
-  // private formTxnMessage(message: any): VersionedMessage {
-  //   if (!message.versioned) {
-  //     return new Message({
-  //       header: {
-  //         numRequiredSignatures: message.header.numRequiredSignatures,
-  //         numReadonlySignedAccounts: message.header.numReadonlySignedAccounts,
-  //         numReadonlyUnsignedAccounts:
-  //           message.header.numReadonlyUnsignedAccounts,
-  //       },
-  //       recentBlockhash: base58.encode(
-  //         Buffer.from(message.recentBlockhash, "base64"),
-  //       ),
-  //       accountKeys: message.accountKeys?.map((d: string) =>
-  //         Buffer.from(d, "base64"),
-  //       ),
-  //       instructions: message.instructions.map(
-  //         ({
-  //           data,
-  //           programIdIndex,
-  //           accounts,
-  //         }: {
-  //           data: any;
-  //           programIdIndex: any;
-  //           accounts: any;
-  //         }) => ({
-  //           programIdIndex: programIdIndex,
-  //           accounts: [...accounts] || [],
-  //           data: base58.encode(Buffer.from(data || "", "base64")),
-  //         }),
-  //       ),
-  //     });
-  //   } else {
-  //     return new MessageV0({
-  //       header: {
-  //         numRequiredSignatures: message.header.numRequiredSignatures,
-  //         numReadonlySignedAccounts: message.header.numReadonlySignedAccounts,
-  //         numReadonlyUnsignedAccounts:
-  //           message.header.numReadonlyUnsignedAccounts,
-  //       },
-  //       recentBlockhash: base58.encode(
-  //         Buffer.from(message.recentBlockhash, "base64"),
-  //       ),
-  //       staticAccountKeys: message.accountKeys.map(
-  //         (k: string) => new PublicKey(Buffer.from(k, "base64")),
-  //       ),
-  //       compiledInstructions: message.instructions.map(
-  //         ({
-  //           programIdIndex,
-  //           accounts,
-  //           data,
-  //         }: {
-  //           programIdIndex: any;
-  //           accounts: any;
-  //           data: any;
-  //         }) => ({
-  //           programIdIndex: programIdIndex,
-  //           accountKeyIndexes: [...accounts] || [],
-  //           data: Uint8Array.from(Buffer.from(data || "", "base64")),
-  //         }),
-  //       ),
-  //       addressTableLookups:
-  //         message.addressTableLookups?.map(
-  //           ({
-  //             accountKey,
-  //             writableIndexes,
-  //             readonlyIndexes,
-  //           }: {
-  //             accountKey: any;
-  //             writableIndexes: any;
-  //             readonlyIndexes: any;
-  //           }) => ({
-  //             writableIndexes: writableIndexes || [],
-  //             readonlyIndexes: readonlyIndexes || [],
-  //             accountKey: new PublicKey(Buffer.from(accountKey, "base64")),
-  //           }),
-  //         ) || [],
-  //     });
-  //   }
-  // }
+  private formTxnMessage(message: any): VersionedMessage {
+    if (!message.versioned) {
+      return new Message({
+        header: {
+          numRequiredSignatures: message.header.numRequiredSignatures,
+          numReadonlySignedAccounts: message.header.numReadonlySignedAccounts,
+          numReadonlyUnsignedAccounts:
+            message.header.numReadonlyUnsignedAccounts,
+        },
+        recentBlockhash: base58.encode(
+          Buffer.from(message.recentBlockhash, "base64"),
+        ),
+        accountKeys: message.accountKeys?.map((d: string) =>
+          Buffer.from(d, "base64"),
+        ),
+        instructions: message.instructions.map(
+          ({
+            data,
+            programIdIndex,
+            accounts,
+          }: {
+            data: any;
+            programIdIndex: any;
+            accounts: any;
+          }) => ({
+            programIdIndex: programIdIndex,
+            accounts:Array.isArray(accounts) ? [...accounts] : [],
+            data: base58.encode(Buffer.from(data || "", "base64")),
+          }),
+        ),
+      });
+    } else {
+      return new MessageV0({
+        header: {
+          numRequiredSignatures: message.header.numRequiredSignatures,
+          numReadonlySignedAccounts: message.header.numReadonlySignedAccounts,
+          numReadonlyUnsignedAccounts:
+            message.header.numReadonlyUnsignedAccounts,
+        },
+        recentBlockhash: base58.encode(
+          Buffer.from(message.recentBlockhash, "base64"),
+        ),
+        staticAccountKeys: message.accountKeys.map(
+          (k: string) => new PublicKey(Buffer.from(k, "base64")),
+        ),
+        compiledInstructions: message.instructions.map(
+          ({
+            programIdIndex,
+            accounts,
+            data,
+          }: {
+            programIdIndex: any;
+            accounts: any;
+            data: any;
+          }) => ({
+            programIdIndex: programIdIndex,
+            accountKeyIndexes:Array.isArray(accounts) ? [...accounts] : [],
+            data: Uint8Array.from(Buffer.from(data || "", "base64")),
+          }),
+        ),
+        addressTableLookups:
+          message.addressTableLookups?.map(
+            ({
+              accountKey,
+              writableIndexes,
+              readonlyIndexes,
+            }: {
+              accountKey: any;
+              writableIndexes: any;
+              readonlyIndexes: any;
+            }) => ({
+              writableIndexes: writableIndexes || [],
+              readonlyIndexes: readonlyIndexes || [],
+              accountKey: new PublicKey(Buffer.from(accountKey, "base64")),
+            }),
+          ) || [],
+      });
+    }
+  }
 
-  // private formMeta(meta: any): ConfirmedTransactionMeta {
-  //   return {
-  //     err: meta.errorInfo ? { err: meta.errorInfo } : null,
-  //     fee: meta.fee,
-  //     preBalances: meta.preBalances,
-  //     postBalances: meta.postBalances,
-  //     preTokenBalances: meta.preTokenBalances || [],
-  //     postTokenBalances: meta.postTokenBalances || [],
-  //     logMessages: meta.logMessages || [],
-  //     loadedAddresses:
-  //       meta.loadedWritableAddresses || meta.loadedReadonlyAddresses
-  //         ? {
-  //             writable:
-  //               meta.loadedWritableAddresses?.map(
-  //                 (address: string) =>
-  //                   new PublicKey(Buffer.from(address, "base64")),
-  //               ) || [],
-  //             readonly:
-  //               meta.loadedReadonlyAddresses?.map(
-  //                 (address: string) =>
-  //                   new PublicKey(Buffer.from(address, "base64")),
-  //               ) || [],
-  //           }
-  //         : undefined,
-  //     innerInstructions:
-  //       meta.innerInstructions?.map(
-  //         (i: { index: number; instructions: any }) => ({
-  //           index: i.index || 0,
-  //           instructions: i.instructions.map((instruction: any) => ({
-  //             programIdIndex: instruction.programIdIndex,
-  //             accounts: instruction.accounts || [],
-  //             data: base58.encode(
-  //               Buffer.from(instruction.data || "", "base64"),
-  //             ),
-  //           })),
-  //         }),
-  //       ) || [],
-  //   };
-  // }
+  private formMeta(meta: any): ConfirmedTransactionMeta {
+    return {
+      err: meta.errorInfo ? { err: meta.errorInfo } : null,
+      fee: meta.fee,
+      preBalances: meta.preBalances,
+      postBalances: meta.postBalances,
+      preTokenBalances: meta.preTokenBalances || [],
+      postTokenBalances: meta.postTokenBalances || [],
+      logMessages: meta.logMessages || [],
+      loadedAddresses:
+        meta.loadedWritableAddresses || meta.loadedReadonlyAddresses
+          ? {
+              writable:
+                meta.loadedWritableAddresses?.map(
+                  (address: string) =>
+                    new PublicKey(Buffer.from(address, "base64")),
+                ) || [],
+              readonly:
+                meta.loadedReadonlyAddresses?.map(
+                  (address: string) =>
+                    new PublicKey(Buffer.from(address, "base64")),
+                ) || [],
+            }
+          : undefined,
+      innerInstructions:
+        meta.innerInstructions?.map(
+          (i: { index: number; instructions: any }) => ({
+            index: i.index || 0,
+            instructions: i.instructions.map((instruction: any) => ({
+              programIdIndex: instruction.programIdIndex,
+              accounts: instruction.accounts || [],
+              data: base58.encode(
+                Buffer.from(instruction.data || "", "base64"),
+              ),
+            })),
+          }),
+        ) || [],
+    };
+  }
 
   private parseTransactionAccounts<T extends Message | VersionedMessage>(
     message: T,
